35ed0cf4347536762c3d3e72c649065a33488d49
Shay Bagants <shayb@jfrog.com>
2015-01-06 09:31:54 +0200

    first commit

---
+++ aaronapp/src/main/java/org/jfrog/aaron/App.java
@@ -0,0 +1,16 @@
+package org.jfrog.aaron;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Gson gson=new Gson();
+    }
+}
